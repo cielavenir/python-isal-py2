beb3941ba2cdf8351096ac9613e9599458045485
T.Yamada <cielartisan@gmail.com>
2022-03-15 00:17:08 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -208,10 +208,10 @@ def build_isa_l(compiler_command, compiler_options):
             shutil.copy(os.path.join(build_dir, "isa-l.h"), os.path.join(temp_prefix, "include", "isa-l.h"))
             #subprocess.check_call(["ls", "bin"])
             os.mkdir(os.path.join(temp_prefix, "lib"))
-            shutil.copy(os.path.join(build_dir, "bin", "libisal.dll"), os.path.join(temp_prefix, "lib", "libisal.dll"))
+            #shutil.copy(os.path.join(build_dir, "bin", "libisal.dll"), os.path.join(temp_prefix, "lib", "libisal.dll"))
             subprocess.check_call(["gcc", "-c", "-o", "bin/chkstk.o", "-m32", "chkstk.S"])
             subprocess.check_call(["gcc", "-c", "-o", "bin/arith64.o", "-m32", "-O2", "arith64.c"])
-            #subprocess.check_call(["ar","cr", os.path.join(temp_prefix, "lib/libisal.a")] + [os.path.join('bin', obj) for obj in os.listdir('bin') if obj.endswith('.o')])
+            subprocess.check_call(["ar","cr", os.path.join(temp_prefix, "lib/libisal.a")] + [os.path.join('bin', obj) for obj in os.listdir('bin') if obj.endswith('.o')])
     elif SYSTEM_IS_WINDOWS:
         with ChDir(build_dir):
             subprocess.run(["nmake", "/E", "/f", "Makefile.nmake"], **run_args)
