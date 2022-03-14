882d224acfe56ac3c0653d65eebd94ea7f96c573
cielavenir <cielartisan@gmail.com>
2022-03-14 23:37:06 +0900

    aaa

--- setup.py
+++ setup.py
@@ -204,7 +204,7 @@ def build_isa_l(compiler_command, compiler_options):
             # we need libisal.a compiled with -fPIC
             # we build .a from slib .o
             ### lol so link fails ###
-            subprocess.check_call(["make", "-f", "Makefile.unx", "-j", str(cpu_count), "arch=noarch", "host_cpu=base_aliases", "DEFINES=-m32", "DEFINES=-m32", "slib", "isa-l.h"], **run_args)
+            subprocess.check_call(["make", "-f", "Makefile.unx", "-j", str(cpu_count), "arch=noarch", "host_cpu=base_aliases", "DEFINES=-m32", "LDFLAGS=-m32", "slib", "isa-l.h"], **run_args)
             shutil.copytree(os.path.join(build_dir, "include"),
                             os.path.join(temp_prefix, "include", "isa-l"))
             shutil.copy(os.path.join(build_dir, "isa-l.h"), os.path.join(temp_prefix, "include", "isa-l.h"))
